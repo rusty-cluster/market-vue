ab0a1c9a77c4a91c06bd44c38f4a5e40b92fb4f5
ayveezub <ayveezub@gmail.com>
2022-09-28 19:49:33 +0300

    add initial version of RetailerOrders

---
+++ src/components/RetailerOrder.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+.retailer-order
+  span.retailer-order__date {{ (new Date(props.timestamp)).toDateString() }}
+  span.retailer-order__name {{ props.name }}
+  span.retailer-order__quantity {{ props.quantity }} wares
+  span.retailer-order__total-price ${{ props.totalPrice }}
+</template>
+
+<script setup>
+const props = defineProps({
+  timestamp: { type: Number, default: 0 },
+  name: { type: String, default: '' },
+  quantity: { type: Number, default: 0 },
+  totalPrice: { type: Number, default: 0 },
+})
+</script>
+
+<style>
+.retailer-order
+  font-size: 28px
+  display: grid
+  grid-template-areas:
+    'date date'
+    'name name'
+    'quantity total-price'
+  align-items: center
+  justify-items: center
+  min-width: 90%
+  margin: 4px
+  padding: 4px
+  border-style: solid
+  border-width: thin
+  border-radius: 4px
+
+span.retailer-order__date
+  grid-area: date
+
+span.retailer-order__name
+  grid-area: name
+  color: #0022ee
+
+span.retailer-order__quantity
+  grid-area: quantity
+
+span.retailer-order__total-price
+  grid-area: total-price
+  color: #118c4f
+</style>
---
+++ src/views/RetailerOrders.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+.retailer-orders
+  RetailerOrder(
+    v-for='stubOrder in stubOrders.slice().reverse()'
+    :timestamp='stubOrder.timestamp'
+    :name='stubOrder.name'
+    :quantity='stubOrder.quantity'
+    :totalPrice='stubOrder.totalPrice'
+    :key='stubOrder.timestamp'
+  )
+</template>
+
+<script setup>
+import RetailerOrder from '@/components/RetailerOrder.vue'
+
+const stubOrders = [
+  {
+    timestamp: 1664369496595,
+    name: '92852402-0009',
+    quantity: 1,
+    totalPrice: 5000
+  },
+  {
+    timestamp: 1664369518373,
+    name: '92852402-0010',
+    quantity: 3,
+    totalPrice: 2500
+  },
+  {
+    timestamp: 1664369529220,
+    name: '92852402-0011',
+    quantity: 1,
+    totalPrice: 12500
+  }
+]
+</script>
+
+<style>
+.retailer-orders
+  height: 100vh
+</style>
